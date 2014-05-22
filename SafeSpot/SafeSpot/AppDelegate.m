2f24b2bc4fc17fca091d71160a0a8edfe543851c
Naveen Yadav <nyadav@u.washington.edu>
2014-05-21 20:07:49 -0700

    testing saving stuff in app delegate

--- SafeSpot/SafeSpot/AppDelegate.m
+++ SafeSpot/SafeSpot/AppDelegate.m
@@ -32,17 +32,13 @@
 {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-    [self.locationManager stopUpdatingLocation];
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application
 {
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    [NSKeyedArchiver archiveRootObject:self.favoritesList
-                                toFile:[self favoritesListStorageLocation]];
-    NSLog(@"Favorites List archived");
-    [self.locationManager stopUpdatingLocation];
+    [self save];
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application
@@ -58,9 +54,19 @@
 - (void)applicationWillTerminate:(UIApplication *)application
 {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    [self save];
+}
+
+// Terminate app, save stuff
+- (void)save
+{
+    [NSKeyedArchiver archiveRootObject:self.favoritesList
+                                toFile:[self favoritesListStorageLocation]];
+    NSLog(@"Favorites List archived");
     [self.locationManager stopUpdatingLocation];
 }
 
+// Archiving Location
 - (NSString *)applicationDocumentsFolderName
 {
     NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
